934f04ffe5eaec0130fc53214c8a7bedcb2d3220
kujio <imkujio@qq.com>
2023-02-06 02:10:16 +0800

    调整数据库位置

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import * as path from 'path'
+import * as url from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,13 +16,16 @@ export default defineConfig({
     server: {
         port: 1420,
         strictPort: true,
+        watch: {
+            ignored: ["**\\*.redb"],
+        },
     },
     // to make use of `TAURI_DEBUG` and other env variables
     // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
     envPrefix: ["VITE_", "TAURI_"],
     build: {
         // Tauri supports es2021
-        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+        target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
         // don't minify for debug builds
         minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
         // produce sourcemaps for debug builds
